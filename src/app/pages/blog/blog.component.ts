26974c19820966be12265197bf78cfcaf91df33d
vCif3r <davidar0601@gmail.com>
2024-04-26 13:37:05 -0500

    correciones

--- src/app/pages/blog/blog.component.ts
+++ src/app/pages/blog/blog.component.ts
@@ -1,12 +1,31 @@
 import { Component } from '@angular/core';
+import { NavbarComponent } from '../../shared/components/navbar/navbar.component';
+import { FooterComponent } from '../../shared/components/footer/footer.component';
 
 @Component({
   selector: 'app-blog',
   standalone: true,
-  imports: [],
+  imports: [NavbarComponent, FooterComponent],
   templateUrl: './blog.component.html',
   styleUrl: './blog.component.css'
 })
 export class BlogComponent {
-
+  blogs = [
+    {
+      img: 'assets/team-1.jpg',
+      title: 'Feliz día del gato',
+    },
+    {
+      img: 'assets/team-2.jpg',
+      title: 'Luna participo en un comercial',
+    },
+    {
+      img: 'assets/team-3.jpg',
+      title: 'Mes de abril incrementaron las adopciones',
+    },
+    {
+      img: 'assets/team-4.jpg',
+      title: 'Después de 3 años de espera, Athenea consiguió un hogar',
+    }
+  ]
 }
--- src/app/pages/testimonios/testimonios.component.ts
+++ src/app/pages/testimonios/testimonios.component.ts
@@ -1,12 +1,66 @@
 import { Component } from '@angular/core';
+import { NavbarComponent } from '../../shared/components/navbar/navbar.component';
+import { FooterComponent } from '../../shared/components/footer/footer.component';
 
 @Component({
   selector: 'app-testimonios',
   standalone: true,
-  imports: [],
+  imports: [NavbarComponent, FooterComponent],
   templateUrl: './testimonios.component.html',
   styleUrl: './testimonios.component.css'
 })
 export class TestimoniosComponent {
+  blogs = [
+    {
+      img: 'assets/perro01.jpeg',
+      name: 'Clifford',
+      description: 'Diam amet eos at no eos sit lorem, amet rebum ipsum clita stet, diam sea est diam eos, rebum sit vero stet justo'
+    },
+    {
+      img: 'assets/perro03.jpg',
+      name: 'Candy',
+      description: 'Diam amet eos at no eos sit lorem, amet rebum ipsum clita stet, diam sea est diam eos, rebum sit vero stet justo'
+    },
+    {
+      img: 'assets/gato04.jpg',
+      name: 'Duquesa',
+      description: 'Diam amet eos at no eos sit lorem, amet rebum ipsum clita stet, diam sea est diam eos, rebum sit vero stet justo'
+    }
+  ]
 
+  hogares = [
+    {
+      img: 'assets/familia01.jpeg',
+      title: 'Maria y Paris',
+      description: 'Diam amet eos at no eos sit lorem, amet rebum ipsum clita stet, diam sea est diam eos, rebum sit vero stet justo'
+    },
+    {
+      img: 'assets/familia02.jpg',
+      title: 'Flor y Chanel',
+      description: 'Diam amet eos at no eos sit lorem, amet rebum ipsum clita stet, diam sea est diam eos, rebum sit vero stet justo'
+    },
+    {
+      img: 'assets/familia03.jpg',
+      title: 'Leny y Fito',
+      description: 'Diam amet eos at no eos sit lorem, amet rebum ipsum clita stet, diam sea est diam eos, rebum sit vero stet justo'
+    }
+  ]
+
+  padrinos = [
+    {
+      img: 'assets/padrino01.jpg',
+      name: 'Grace',
+      description: 'Diam amet eos at no eos sit lorem, amet rebum ipsum clita stet, diam sea est diam eos, rebum sit vero stet justo'
+    },
+    {
+      img: 'assets/padrino02.jpg',
+      name: 'Roberth',
+      description: 'Diam amet eos at no eos sit lorem, amet rebum ipsum clita stet, diam sea est diam eos, rebum sit vero stet justo'
+    },
+    {
+      img: 'assets/padrino03.jpg',
+      name: 'Karina',
+      description: 'Diam amet eos at no eos sit lorem, amet rebum ipsum clita stet, diam sea est diam eos, rebum sit vero stet justo'
+    }
+  ]
 }
