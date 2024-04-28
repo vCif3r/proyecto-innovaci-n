534984c63b6bfb8db7050092fdf490846dde6b66
vCif3r <davidar0601@gmail.com>
2024-04-28 15:37:02 -0500

    navbar

--- src/app/shared/components/navbar/navbar.component.ts
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -1,7 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 import { faUser } from '@fortawesome/free-solid-svg-icons';
+import { IUsuario } from '../../../core/models/Usuario.model';
+import { AuthService } from '../../../core/services/auth.service';
 
 @Component({
   selector: 'app-navbar',
@@ -10,6 +12,17 @@ import { faUser } from '@fortawesome/free-solid-svg-icons';
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
   faUser = faUser
+
+  usuario?: any;
+  constructor(private _auth: AuthService){}
+
+  ngOnInit(): void {
+    this.usuario = this._auth.getCurrentUser();
+  }
+
+  logout() {
+    this._auth.logoutUser();
+  }
 }
